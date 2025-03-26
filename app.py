f6ffe28530fa8f2a26aa769ea5433b56ac2b05a6
Roberto-batman <143817529+Roberto-batman@users.noreply.github.com>
2025-03-26 09:32:16 -0400

    edited credit

--- app.py
+++ app.py
@@ -210,6 +210,7 @@ else:
 
 with st.sidebar:
     st.divider()
+    st.write("Original creator credit goes to Enric Domingo.  Here are some of his links: ")
     st.video("https://youtu.be/abMwFViFFhI")
     st.write("📋[Medium Blog](https://medium.com/@enricdomingo/program-a-rag-llm-chat-app-with-langchain-streamlit-o1-gtp-4o-and-claude-3-5-529f0f164a5e)")
     st.write("📋[GitHub Repo](https://github.com/enricd/rag_llm_app)")
